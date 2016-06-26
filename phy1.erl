5e21f1a6e33db46f7a4a7d5f9c85c9dc1c9bfbef
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2016-06-26 07:18:45 +0300

    the model with friction verified

--- phy1.erl
+++ phy1.erl
@@ -12,7 +12,7 @@
 
 -define(L,  0.1). % leg's length
 -define(M, 0.10). % the mass of the body
--define(K0, 0.4). % friction in the shoulder
+-define(K0, 0.02). % friction in the shoulder
 
 %% springs params
 -define(Z0, 0.025).
@@ -27,13 +27,13 @@
 
 init() -> init(1.5).
 
-init(Alpha) -> {Alpha, Alpha, 0.0}.
+init(Alpha) -> {Alpha, 0.0, 0.0}.
 
 
-next_position({A,A_old,Psi},New_Psi,Tau) ->
+next_position({A,A_der,Psi},New_Psi,Tau) ->
 	% consider estimating energy injection or consumption
-	A_new = 2*A - A_old + f1(A,Psi)*Tau*Tau - ?K0*(A-A_old)*Tau,
-	{A_new, A, New_Psi}.
+	A_new = A + A_der*Tau + (f1(A,Psi) - ?K0*A_der)*Tau*Tau/?M/?L/?L,
+	{A_new, (A_new-A)/Tau, New_Psi}.
 
 %
 %
